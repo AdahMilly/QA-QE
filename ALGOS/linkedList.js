bd47ed1181e0197d64d1fe787b573645ae204a6a
AdahMilly <oamildred@gmail.com>
2023-10-25 20:36:33 +0300

    notes

---
+++ ALGOS/linkedList.js
@@ -0,0 +1,4 @@
+// made of nodes, each node stores a value, a seguence of element where one elemnet has a sequesnce to the next element in the list
+// teh 1st element is called the head, while the last element is called the tail.
+// the node class represents a single node in a linked list.It has two properties which are data and next.
+// the data property is used to store the actual data of the node, whereas the next property is a refernce to the next node in the list
